48a90b6191308f7316edc8d777e6c68be99b039d
stefuma19 <stefano14.fumagalli@mail.polimi.it>
2021-05-12 18:16:36 +0200

    Aggiunti beans Fornitore, Prodotto, Range

--- Progetto-TIW-2021/src/it/polimi/tiw/progetto/beans/Fornitore.java
+++ Progetto-TIW-2021/src/it/polimi/tiw/progetto/beans/Fornitore.java
@@ -1,5 +1,46 @@
 package it.polimi.tiw.progetto.beans;
 
+import java.util.List;
+
 public class Fornitore {
 
+	private int ID;
+	private String nome;
+	private float valutazione; 
+	private int soglia;
+	private List<Range> politica;
+	
+	
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public float getValutazione() {
+		return valutazione;
+	}
+	public void setValutazione(float valutazione) {
+		this.valutazione = valutazione;
+	}
+	public int getSoglia() {
+		return soglia;
+	}
+	public void setSoglia(int soglia) {
+		this.soglia = soglia;
+	}
+	public List<Range> getPolitica() {
+		return politica;
+	}
+	public void setPolitica(List<Range> politica) {
+		this.politica = politica;
+	}
+	
+	
 }
