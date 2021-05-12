36f1f51a4a063a2d2a9371814a4bc2003d4f19e2
aralara <lara.ferro@mail.polimi.it>
2021-05-12 18:17:23 +0200

    Implementati get e set della classe Utente

--- Progetto-TIW-2021/src/it/polimi/tiw/progetto/beans/Utente.java
+++ Progetto-TIW-2021/src/it/polimi/tiw/progetto/beans/Utente.java
@@ -2,4 +2,40 @@ package it.polimi.tiw.progetto.beans;
 
 public class Utente {
 
+	private String e_mail;
+	private String nome;
+	private String cognome;
+	private String indirizzo; //TODO: come string?
+	
+	public String getE_mail() {
+		return e_mail;
+	}
+	
+	public void setE_mail(String e_mail) {
+		this.e_mail = e_mail;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCognome() {
+		return cognome;
+	}
+	
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+	
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
 }
