273e0101057caf30b26adaf789d786e2b9538d19
stefuma19 <stefano14.fumagalli@mail.polimi.it>
2021-05-31 19:14:18 +0200

    Aggiunta gestione cookie, manca il parsing e cambiare il fragment

--- Progetto-TIW-2021/src/it/polimi/tiw/progetto/controllers/GoToCarrello.java
+++ Progetto-TIW-2021/src/it/polimi/tiw/progetto/controllers/GoToCarrello.java
@@ -1,18 +1,24 @@
 package it.polimi.tiw.progetto.controllers;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
+import org.thymeleaf.standard.serializer.StandardJavaScriptSerializer;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import com.mysql.cj.Session;
 
 @WebServlet("/GoToCarrello")
 public class GoToCarrello extends HttpServlet{
@@ -34,7 +40,34 @@ public class GoToCarrello extends HttpServlet{
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		boolean primo=true;  //se dobbiamo creare un cookie per il fornitore
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null) {
+			for (int i = 0; i < cookies.length; i++) {
+				Cookie c = cookies[i];
+				String nome = c.getName();
+				if(nome.equals(request.getParameter("IdFor"))) {
+					primo = false;
+					String valore = c.getValue(); //TODO: controllo se ho già comprato quel prodotto e ne aumento solo la quantita, serve il parser
+					valore += "_" + request.getParameter("IdProd") + "-" + request.getParameter("quantita");
+					Cookie coo = new Cookie(nome, valore);
+					coo.setMaxAge(3600);
+					response.addCookie(coo);
+					break;
+				}
+			}
+		}
 
+		if(primo) {
+			String idFor = request.getParameter("IdFor");
+			String nome = idFor;
+			String valore = request.getParameter("IdProd") + "-" + request.getParameter("quantita");
+			Cookie coo = new Cookie(nome, valore);
+			coo.setMaxAge(3600);
+			response.addCookie(coo);
+		} 
+		
+		
 		String path = "/WEB-INF/carrello.html";
 		ServletContext servletContext = getServletContext();
 		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
